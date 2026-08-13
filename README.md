8dd057e16ba04a1b3e2cbf16b6e250edeb28cee4
Thiago Faria <tgroyale@gmail.com>
2026-08-13 18:30:12 -0300

    First commit, things done: I created ConnectDB, CreateTables functions

---
+++ README.md
@@ -0,0 +1,8 @@
+# English is not my first language. Forgive my errors.🙏
+# TodoLite
+Is a simple CLI to-do list app made in Go lang.
+The sole porpouse of this is training my programming logic and Go lang skills.
+I will not use any AI to program for me. Instead, I will use to review my code and give me feedbacks when I'm done.
+
+## Libraries
+TodoLite uses `modernc.org/slqlite` and `github.com/spf13/cobra`
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"database/sql"
+	_ "modernc.org/sqlite"
+)
+
+type Domain struct {
+	ID int
+	Name string
+}
+
+type Task struct {
+	ID int
+	Title string
+	Done bool
+	DomainID int
+}
+
+func ConnectDB(dbname string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dbname)
+	if err != nil {
+		fmt.Println("Failed to open database:", err)
+		return nil, err
+	}
+
+	if db == nil {
+		fmt.Println("Database is nil")
+		return nil, errors.New("Database is nil")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Failed to ping database:", err)
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func CreateTables(db *sql.DB) error {
+	if db == nil {
+		fmt.Println("Bad argument to CreateTables: database is nil.")
+		return errors.New("Database is nil")
+	}
+
+	err := db.Ping()
+	if err != nil {
+		fmt.Println("Failed to ping database:", err)
+		return err
+	}
+
+	TaskTableSQL := `
+		CREATE TABLE IF NOT EXISTS tasks (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			done BOOL NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS domains (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT UNIQUE NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS task_domain (
+			task_id INTEGER,
+			domain_id INTEGER,
+			PRIMARY KEY (task_id, domain_id),
+			FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
+			FOREIGN KEY (domain_id) REFERENCES domains(id) ON DELETE CASCADE
+		);
+	`
+
+	stmt, err := db.Prepare(TaskTableSQL)
+	if err != nil {
+		fmt.Println("Failed to prepare SQL tables creation statement:", err)
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec()
+	if err != nil {
+		fmt.Println("Failed to exec table creation SQL statement:", err)
+		return err
+	}
+
+	return nil
+}
+
+func PrintTasks() {}
+
+func AddTask() {}
+
+func main() {
+
+}
